f7baab72e2f384f6a99547800e5aeeb29c70eeee
Arnab <arnabroy741@gmail.com>
2021-09-28 09:29:17 +0530

    sample py code for jenkins pipeline test

---
+++ test.py
@@ -0,0 +1,18 @@
+import os
+import copy
+
+spath = r"C:\Users\Arnab\Desktop\JAVASCRIPT"
+for roots, dirs, files in os.walk(spath, topdown = True):
+    origLen = len(dirs)
+    dirs[:] = [dirr for dirr in dirs if len(os.path.join(roots,dirr)) < 50]
+    if origLen != len(dirs):
+        print('HeHe\n')
+    print(roots, dirs, files)
+    print('******************')
+    '''
+    if roots[len(spath):].count(os.sep) >= 2:
+        dirs.clear()
+    for file in files:
+        print(os.path.join(roots,file))
+        '''
+print(os.environ['KIERMANFS'])
